0f6ce7114293d3d45b958146cde12c5f21195d3f
kolotyn23 <Byratino23@mail.ru>
2026-02-17 22:30:39 +0300

    Chapter 2 complete
    Rewrite Home controller to WebMvcConfigurer interface.

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
